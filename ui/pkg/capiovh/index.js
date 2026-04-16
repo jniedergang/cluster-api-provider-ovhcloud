a682afa690ac7ec9c314b454b070b02962bc5d7a
jniedergang <julien_niedergang@hotmail.com>
2026-04-16 08:42:57 +0200

    fix(ui): add debug logging to extension

--- ui/pkg/capiovh/index.js
+++ ui/pkg/capiovh/index.js
@@ -1,3 +1,7 @@
+console.log('[CAPIOVH] Extension loading...');
+
 export default function(plugin) {
+  console.log('[CAPIOVH] Plugin init called', plugin);
   plugin.addProduct(require('./product'));
+  console.log('[CAPIOVH] Product added');
 }
